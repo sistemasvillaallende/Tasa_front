bd73bcf503bb023c8d62187055879979bab93e7a
Facundo Uferer <juanfacundouf@gmail.com>
2025-03-03 16:27:46 -0300

    merjoa page not found

---
+++ src/pages/Errors/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <h1 className="text-3xl font-bold">Error 404: Page Not Found</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
